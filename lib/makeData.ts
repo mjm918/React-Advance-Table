3b8c8dc746b3aca54c1e07b698082453e2a5a8e5
Mohammad Julfikar <jxmxshakil@gmail.com>
2024-05-08 12:16:51 +0800

    - removed React Query - ui changes - planning to add form + zod

--- lib/makeData.ts
+++ lib/makeData.ts
@@ -1,5 +1,4 @@
 import { faker } from '@faker-js/faker'
-import {TAdvancedDataTableDataProps} from "@/components/data-table";
 
 export type Person = {
 	firstName: string
@@ -46,23 +45,4 @@ export function makeData(...lens: number[]) {
 	}
 
 	return makeDataLevel()
-}
-
-const data = makeData(100_000)
-
-export async function simulateFetch(options: {
-	pageIndex: number
-	pageSize: number
-}): Promise<TAdvancedDataTableDataProps<Person>> {
-	// Simulate some network latency
-	await new Promise(r => setTimeout(r, 1000))
-
-	return {
-		rows: data.slice(
-			options.pageIndex * options.pageSize,
-			(options.pageIndex + 1) * options.pageSize
-		),
-		pageCount: Math.ceil(data.length / options.pageSize),
-		rowCount: data.length,
-	}
 }
